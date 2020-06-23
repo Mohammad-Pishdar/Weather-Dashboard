f5d3a504d35a0e4b16bab03a860b6e91a87c2722
Mo Pishdar <mohpish@gmail.com>
2020-06-23 17:12:58 +1000

    all the basic weather data for a location is now available

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const currentDateSpan = $(".current-date");
-currentDateSpan.text("(" + moment().format('l') + ")");
+const cityAndDate = $(".cityAndDate");
+const temperatureSpan = $(".temperature-span");
+const humiditySpan = $(".humidity-span");
+const windSpeedSpan = $(".windSpeedSpan");
+const crrentUvIndexSpan = $(".currentUvIndexSpan");
+const startUpQuery = "http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=c5d7b2e683d4e3a0a888a7df1a7043fc"
+
+$.ajax({
+    url: startUpQuery,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    cityAndDate.text(response.name + " " + "(" + moment().format('l') + ")");
+    temperatureSpan.text(((response.main.temp) - 273.15).toFixed(1));
+    humiditySpan.text(response.main.humidity);
+    windSpeedSpan.text((response.wind.speed * 1.609).toFixed(1));
+    let longitude = response.coord.lon;
+    let latitude = response.coord.lat;
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/uvi?appid=c5d7b2e683d4e3a0a888a7df1a7043fc&lat=" + latitude + "&lon=" + longitude,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        crrentUvIndexSpan.text(response.value);
+    })
+});
