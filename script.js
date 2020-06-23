a83aff596903a53813c87bae7bf4348887360d9a
Mo Pishdar <mohpish@gmail.com>
2020-06-23 17:25:17 +1000

    weather condition icon now added after the name of the city

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const temperatureSpan = $(".temperature-span");
 const humiditySpan = $(".humidity-span");
 const windSpeedSpan = $(".windSpeedSpan");
 const crrentUvIndexSpan = $(".currentUvIndexSpan");
+const weatherIcon = $(".weatherIcon");
 const startUpQuery = "http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=c5d7b2e683d4e3a0a888a7df1a7043fc"
 
 $.ajax({
@@ -11,6 +12,8 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
     cityAndDate.text(response.name + " " + "(" + moment().format('l') + ")");
+    weatherIcon.attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon +
+        ".png")
     temperatureSpan.text(((response.main.temp) - 273.15).toFixed(1));
     humiditySpan.text(response.main.humidity);
     windSpeedSpan.text((response.wind.speed * 1.609).toFixed(1));
