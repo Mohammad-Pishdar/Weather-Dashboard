b3bb205a14950f19d9f0ee85f54b32b68a086a31
Mo Pishdar <mohpish@gmail.com>
2020-06-24 16:08:54 +1000

    changed http query calls to https so it can correctly be edeployed over https guthub servers

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ const searchInputForm = $(".form-control");
 const submitButton = $("#submit-button");
 
 function currentWeather(city) {
-    let queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c5d7b2e683d4e3a0a888a7df1a7043fc&units=metric"
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c5d7b2e683d4e3a0a888a7df1a7043fc&units=metric"
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
         cityAndDate.text(response.name + " " + "(" + moment().format('l') + ")");
-        weatherIcon.attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png")
+        weatherIcon.attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png")
         temperatureSpan.text((response.main.temp).toFixed(1));
         humiditySpan.text(response.main.humidity);
         windSpeedSpan.text(response.wind.speed);
         let longitude = response.coord.lon;
         let latitude = response.coord.lat;
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/uvi?appid=c5d7b2e683d4e3a0a888a7df1a7043fc&lat=" + latitude + "&lon=" + longitude,
+            url: "https://api.openweathermap.org/data/2.5/uvi?appid=c5d7b2e683d4e3a0a888a7df1a7043fc&lat=" + latitude + "&lon=" + longitude,
             method: "GET"
         }).then(function (response) {
             crrentUvIndexSpan.text(response.value);
@@ -45,34 +45,34 @@ function currentWeather(city) {
 }
 
 function fiveDayForecast(city) {
-    let queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c5d7b2e683d4e3a0a888a7df1a7043fc&units=metric";
+    let queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c5d7b2e683d4e3a0a888a7df1a7043fc&units=metric";
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
         console.log(response);
         $("#day1date").text(moment().add(1, 'days').format('l'));
-        $("#day1Icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + ".png");
+        $("#day1Icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + ".png");
         $("#day1temperature").text(response.list[1].main.temp);
         $("#day1Humidity").text(response.list[1].main.humidity);
 
         $("#day2date").text(moment().add(2, 'days').format('l'));
-        $("#day2Icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[9].weather[0].icon + ".png");
+        $("#day2Icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[9].weather[0].icon + ".png");
         $("#day2temperature").text(response.list[9].main.temp);
         $("#day2Humidity").text(response.list[9].main.humidity);
 
         $("#day3date").text(moment().add(3, 'days').format('l'));
-        $("#day3Icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[17].weather[0].icon + ".png");
+        $("#day3Icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[17].weather[0].icon + ".png");
         $("#day3temperature").text(response.list[17].main.temp);
         $("#day3Humidity").text(response.list[17].main.humidity);
 
         $("#day4date").text(moment().add(4, 'days').format('l'));
-        $("#day4Icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[25].weather[0].icon + ".png");
+        $("#day4Icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[25].weather[0].icon + ".png");
         $("#day4temperature").text(response.list[25].main.temp);
         $("#day4Humidity").text(response.list[25].main.humidity);
 
         $("#day5date").text(moment().add(5, 'days').format('l'));
-        $("#day5Icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[33].weather[0].icon + ".png");
+        $("#day5Icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[33].weather[0].icon + ".png");
         $("#day5temperature").text(response.list[33].main.temp);
         $("#day5Humidity").text(response.list[33].main.humidity);
 
